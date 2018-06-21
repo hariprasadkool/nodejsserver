576272cd27c188249691a0177eb5b990052686f4
hariprasadkool <hariprasad.g@techpearl.com>
2018-06-21 16:17:54 +0530

    added code for browser options call to check and make another call again

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const welcomepage = require('./welcomepage.js');
 
 // Create HTTP server and listen on port 8000 for requests
 http.createServer(function (request, response) {
+    if(request.method === 'OPTIONS'){
+        console.log('!OPTIONS');
+        const headers = {};
+        // IE8 does not allow domains to be specified, just the *
+        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+        headers["Access-Control-Allow-Origin"] = "*";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        response.writeHead(200, headers);
+        response.end();
+    }
     if (request.method === 'POST') {
         let body = '';
         request.on('data', chunk => {
