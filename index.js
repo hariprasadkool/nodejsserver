cc19f2b2606c8e6a5ee0587f6e98efa921d5d701
hariprasadkool <hariprasad.g@techpearl.com>
2018-06-21 13:43:21 +0530

    using builtin http module created the server & send empty response back

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+
+//Create HTTP server and listen on port 8000 for requests
+http.createServer(function (request, response) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // sending the response back and ending the connection
+   response.end();
+
+}).listen(9000);
+
+
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:9000/')
